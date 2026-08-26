56b4390eae276f9a3e82f0885534305c40666fb9
faza76 <fazakbar76@gmail.com>
2026-08-26 23:10:55 +0700

    reconfig route for blog path

--- src/App.svelte
+++ src/App.svelte
@@ -5,22 +5,33 @@
   import Home from './lib/pages/Home.svelte';
   import About from './lib/pages/About.svelte';
   import Blog from './lib/pages/Blog.svelte';
+  import BlogPost from './lib/pages/BlogPost.svelte';
   import Contact from './lib/pages/Contact.svelte';
 
-  const pages = {
-    '/': Home,
-    '/about': About,
-    '/blog': Blog,
-    '/contact': Contact,
-  };
+  function resolvePage(path) {
+    if (path === '/') return Home;
+    if (path === '/about') return About;
+    if (path === '/blog' || path === '/blog/') return Blog;
+    if (path.startsWith('/blog/')) return BlogPost;
+    if (path === '/contact') return Contact;
+    return Home;
+  }
 
-  $: activePage = pages[$route] ?? Home;
+  // Extract the post slug from a /blog/<slug> route. Strips any trailing slash.
+  $: slug = $route.startsWith('/blog/')
+    ? $route.slice('/blog/'.length).replace(/\/+$/, '')
+    : '';
+  $: activePage = resolvePage($route);
 </script>
 
 <Nav />
 
 <main class="flex-grow pt-24">
-  <svelte:component this={activePage} />
+  {#if activePage === BlogPost}
+    <BlogPost {slug} />
+  {:else}
+    <svelte:component this={activePage} />
+  {/if}
 </main>
 
 <Footer />
